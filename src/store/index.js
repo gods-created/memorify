6ec3b33ec725b4c5b11a8bbd1328f9d942a8238f
B. Terskow <tersk.bo@gmail.com>
2024-09-25 18:02:56 +0300

    edit server url
    Signed-off-by: B. Terskow <tersk.bo@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'vuex'
 const store = createStore({
   state () {
     return {
-      server_url: 'https://d3mqh3tp92cpd9.cloudfront.net',
+      server_url: 'https://d1z32plj7ieyo0.cloudfront.net',
       bucket_url: 'https://memorify-2024.s3.amazonaws.com',
       admin_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
     }
